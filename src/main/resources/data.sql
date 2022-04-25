8fc706575bd7b651ce7b26dcf412c486e0b05256
Ilya <zhuravlevpticaigorevich@yandex.ru>
2022-04-25 10:56:33 +0300

    add init and populate scripts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,48 @@
+delete from USERS;
+delete from USER_ROLES;
+delete from RESTAURANT;
+delete from DISH;
+delete from VOTE;
+
 INSERT INTO USERS (name, email, password)
 VALUES ('User', 'user@yandex.ru', '{noop}password'),
-       ('Admin', 'admin@gmail.com', '{noop}admin');
+       ('Admin', 'admin@gmail.com', '{noop}admin'),
+       ('User2', 'user2@yandex.ru', '{noop}328328');
 
 INSERT INTO USER_ROLES (role, user_id)
 VALUES ('USER', 1),
        ('ADMIN', 2),
-       ('USER', 2);
+       ('USER', 2),
+       ('USER', 3);
+
+insert into RESTAURANT (name)
+values ('Duo Gastrobar'),
+       ('tartarbar'),
+       ('Duo Asia'),
+       ('Мастер Кебаб');
+
+insert into DISH (restaurant_id, title, cost)
+values (1, 'Тартар из говядины с хумусом и пармезаном', 490),
+       (1, 'Паста орзо с шеей бычка', 490),
+       (1, 'Говядина Топ-блейд с трюфельным пюре', 590),
+       (1, 'Чернослив с соленой карамелью и орехом пекан', 390),
+       (2, 'Тартар из говядины со шпинатом и трюфельным понзу', 490),
+       (2, 'Тартар из говядины с пармезаном', 550),
+       (2, 'Тартар из оленины с соусом из оливок и халапеньо', 550),
+       (2, 'Севиче из тунца', 590),
+       (2, 'Татаки из говядины с фуа-гра', 850),
+       (3, 'Сашими из лосося с трюфельным соусом и свежим хреном', 590),
+       (3, 'Сашими из тунца с васаби', 550),
+       (3, 'Татаки из говядины с кимчи', 550),
+       (3, 'Гёдза с грибами и трюфельным соусом', 490),
+       (3, 'Шея бычка в устричном соусе', 750);
+
+insert into VOTE (user_id, voting_date, restaurant_id)
+values (1, current_date-3, 1),
+       (2, current_date-3, 2),
+       (1, current_date-2, 3),
+       (2, current_date-2, 3),
+       (1, current_date-1, 4),
+       (2, current_date-1, 1),
+       (1, current_date,   2),
+       (2, current_date,   2);
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,53 @@
+drop table if exists USERS cascade;
+drop table if exists RESTAURANT cascade;
+drop table if exists USER_ROLES;
+drop table if exists VOTE;
+drop table if exists DISH;
+
+create table USERS
+(
+    id          int         primary key     auto_increment,
+    name        varchar     not null,
+    email       varchar     not null,
+    password    varchar     not null,
+    enabled     boolean     not null        default true,
+    registered  timestamp   not null        default now()
+);
+create unique index USERS_UNIQUE_EMAIL_IDX on USERS (email);
+
+create table USER_ROLES
+(
+    user_id     int         not null,
+    role        varchar     not null,
+    foreign key (user_id) references USERS (id) on delete cascade,
+    constraint ROLES_UNIQUE_USERID_ROLE_CONSTRAINT unique (user_id, role)
+);
+
+create table RESTAURANT
+(
+    id          int         primary key     auto_increment,
+    name        varchar     not null,
+    constraint RESTAURANTS_UNIQUE_NAME unique (name)
+);
+
+create table DISH
+(
+    id              int         primary key     auto_increment,
+    restaurant_id   int         not null,
+    title           varchar     not null,
+    cost            int         not null,
+    foreign key (restaurant_id) references RESTAURANT (id) on delete cascade,
+    constraint DISHES_UNIQUE_RESTAURANT_ID_TITLE_CONSTRAINT unique (restaurant_id, title)
+);
+
+create table VOTE
+(
+    id              int         primary key     auto_increment,
+    user_id         int         not null,
+    voting_date     date        not null        default current_date,
+    restaurant_id   int         not null,
+    foreign key (user_id) references USERS (id) on delete cascade,
+    foreign key (restaurant_id) references RESTAURANT (id) on delete cascade,
+    constraint VOTES_UNIQUE_USERID_VOTING_DATE_CONSTRAINT unique (user_id, voting_date)
+);
+create index VOTES_DATE_IDX on VOTE (voting_date);
